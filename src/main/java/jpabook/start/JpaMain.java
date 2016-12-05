3c0a2efb04a69c4f0068afad9ec1d2066f2f6e0a
jaegyu <jpmlyr@gmail.com>
2016-12-05 12:53:36 +0900

    JPA실

---
+++ src/main/java/jpabook/start/JpaMain.java
@@ -0,0 +1,54 @@
+package jpabook.start;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			logic(em);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.clear();
+		}
+		emf.close();
+	}
+
+	private static void logic(EntityManager em) {
+		String id = "id1";
+		Member member = new Member();
+		member.setId(id);
+		member.setUsername("예일");
+		member.setAge(1);
+
+		// 등록
+		em.persist(member);
+
+		// 수정
+		member.setAge(20);
+
+		// 한건 조회
+		Member findMember = em.find(Member.class, id);
+		System.out.printf("findMember=%s, age=%\n", findMember.getUsername(), findMember.getAge());
+
+		// 목록조회
+		List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+		System.out.printf("members.size=%d\n", members.size());
+		
+		em.remove(member);
+	}
+}
---
+++ src/main/java/jpabook/start/Member.java
@@ -0,0 +1,27 @@
+package jpabook.start;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+
+	@Id
+	@Column(name = "ID")
+	private String id;
+
+	@Column(name = "NAME")
+	private String username;
+
+	// @Column(name = "AGE")
+	private Integer age;
+
+}
