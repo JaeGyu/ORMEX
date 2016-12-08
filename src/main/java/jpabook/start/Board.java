194c5afb10e144369b5b9d6d18cad7132e6084a5
jaegyu <jpmlyr@gmail.com>
2016-12-08 11:44:28 +0900

    JPA 실습

--- src/main/java/jpabook/start/Board.java
+++ src/main/java/jpabook/start/Board.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
 
 import lombok.Getter;
 import lombok.ToString;
@@ -15,13 +16,17 @@ import lombok.Setter;
 @Setter
 @ToString
 @Entity(name = "board")
-@SequenceGenerator(name = "BOARD_SEQ_GENERATOR", sequenceName = "BOARD_SEQ", initialValue = 1, allocationSize = 20)
-//allocationSize <- 이 옵션이 적용이 안됨 H2 db라서 그런것인지...  
+// @SequenceGenerator(name = "BOARD_SEQ_GENERATOR", sequenceName = "BOARD_SEQ",
+// initialValue = 1, allocationSize = 20)
+@TableGenerator(name = "BOARD_SEQ_GENERATOR", table = "MY_SEQUENCES", pkColumnValue = "BOARD_SEQ", allocationSize = 1)
+// allocationSize <- 이 옵션이 적용이 안됨 H2 db라서 그런것인지...
 public class Board {
 
 	@Id
 	// @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+	// @GeneratedValue(strategy = GenerationType.SEQUENCE, generator =
+	// "BOARD_SEQ_GENERATOR")
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
 	private Long id;
 
 	@Column(name = "data", length = 255)
